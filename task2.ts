4da8693cd0713e3dc90aecfadeb432cae0c8b2ff
Shahzaib786 <125633854+shahzaibee@users.noreply.github.com>
2023-03-16 14:00:58 +0500

    Update task2.ts

--- task2.ts
+++ task2.ts
@@ -1,3 +1,10 @@
+•	Declare an array of strings, e.g. ["apple", "banana", "cherry", "date", "elderberry"]
+•	Use array methods to perform the following manipulations:
+o	Append a string to the end of the array
+o	Prepend a string to the beginning of the array
+o	Remove a string from a specific index in the array and replace it with another string
+•	Display the array before and after each manipulation to the user as output.
+
 // 2- Task: Create an Array Manipulation Program
 
 // · Use array methods to perform the following manipulations:
